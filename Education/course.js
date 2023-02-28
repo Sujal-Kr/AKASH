a9d701ce94978601ba2e5eec0dcaba311515252f
Sujal-Kr <85947816+Sujal-Kr@users.noreply.github.com>
2023-02-28 16:19:48 +0530

    Add files via upload

---
+++ Education/course.js
@@ -0,0 +1,29 @@
+const courses=[
+    {
+        courses:"Bsc-It",
+        Materials:"Pdf Ques-Paper",
+        image:"url('Assets/Designer _Outline.svg')",
+
+    }
+    ,
+    {
+        courses:"Bsc-Maths",
+        Materials:"Pdf Ques-Paper",
+        image:"url('Assets/Designer _Outline.svg')",
+        
+    }
+    ,
+    {
+        courses:"BFMO",
+        Materials:"Pdf Ques-Paper",
+        image:"url('Assets/Designer _Outline.svg')",
+        
+    },
+    {
+        courses:"BBA",
+        Materials:"Pdf Ques-Paper",
+        image:"url('Assets/Designer _Outline.svg')",
+        
+    },
+    
+]
